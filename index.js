d1a0cb83a00258be1c5050935a181dc68a4f8536
Intekhabilyas <intekhabilyas001@gmail.com>
2024-04-06 19:36:52 +0530

    change index.js

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const express=require("express")
-const app=express()
-const axios=require("axios")
-const cheerio=require("cheerio")
-const PORT=8000
-const challenges=[]
+const PORT = 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
 
-app.get("/",async(req,res)=>{
-res.json({message:"hello welcome to our stream"})
+const challenges = []
+
+app.get('/', (req, res) => {
+    res.json('Welcome to GHW Stream demo')
 })
 
-app.get("/challenges",async (req,res)=>{
-    axios.get("https://www.ghw.mlh.io/challenges")
-    .then(response=>{
-        const html=response.data
-        const $= cheerio.load(html)
+app.get('/challenges', (req, res) => {
 
-        $('a.contains("")'.html).each(function(){
-            const title=$(this).text()
-            const url=$(this).attr("href")
 
-            challenges.push({
-                title,
-                url
-            })
-        })
-           
+
+    axios.get('https://ghw.mlh.io/challenges')
+            .then(response => {
+                const html = response.data
+                const $ = cheerio.load(html)
+                
+    
+                $('a:contains("")', html).each(function () {
+                    const title = $(this).text()
+                    const url = $(this).attr('href')
+                    challenges.push({
+                        title,
+                        url
+                    })        
+                })
+                res.json(challenges)
+            }).catch(err => console.log(err))
+    })
+    
     
 
-        res.json(challenges)
-    })
-})
-app.listen(PORT,()=>{
-    console.log(`listening to http://localhost:${PORT}`)
-})
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
